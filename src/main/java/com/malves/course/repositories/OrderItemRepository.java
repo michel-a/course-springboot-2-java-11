6e7414670379952c9fb13167c48e4bd98d9c431e
michel-a <malvesdesign@hotmail.com>
2020-06-22 11:51:53 -0300

    OrderItem, many-to-many association with extra attributes PARTE 2

---
+++ src/main/java/com/malves/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.malves.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.malves.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
